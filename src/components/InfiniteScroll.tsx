c42316077d35d1f07ac153fad46f8648c5ed1494
EricChavarria <eric.chavarria1409@gmail.com>
2024-12-07 15:33:54 -0600

    fix styles match tables

--- src/components/InfiniteScroll.tsx
+++ src/components/InfiniteScroll.tsx
@@ -1,76 +1,32 @@
-import React, { SyntheticEvent, ReactNode } from 'react';
+const SkeletonRow = ({ elementLength }: { elementLength: number }) => {
+  const template = [1, 2, 3, 4, 5];
 
-const SkeletonRow = ({ tableCount }: { tableCount: number }) => {
-  const template = [1, 2];
   return (
-    <table className="min-w-[651.36px] w-full">
-      <tbody>
-        {template.map((_, index) => {
-          const bgPulse =
-            (tableCount + index) % 2 === 0 ? 'bg-white' : 'bg-gray-300';
-          const bgTr =
-            (tableCount + index) % 2 !== 0 ? 'bg-white' : 'bg-gray-300';
-          return (
-            <tr key={index} className={`${bgTr} flex `}>
-              <td className="py-2 px-4  border-red-200 border w-1/12">
-                <div
-                  className={`${bgPulse} h-4 rounded w-10 animate-pulse `}
-                ></div>
-              </td>
-              <td className="py-2 pr-4  border border-green-200 w-5/12">
-                <div className={`${bgPulse} h-4 rounded animate-pulse `}></div>
-              </td>
-              <td className="py-2 px-4  border-red-500 border w-5/12">
-                <div
-                  className={`${bgPulse} h-4 rounded w-60 animate-pulse`}
-                ></div>
-              </td>
-              <td className="py-2 px-4  border border-yellow-300 w-1/12">
-                <div className={`${bgPulse} h-4 rounded animate-puls`}></div>
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <>
+      {template.map((_, index) => {
+        const bgPulse =
+          (elementLength + index) % 2 === 0 ? 'bg-white' : 'bg-gray-300';
+        const bgTr =
+          (elementLength + index) % 2 !== 0 ? 'bg-white' : 'bg-gray-300';
+        return (
+          <tr key={index} className={`${bgTr}`}>
+            <td className="py-2 px-4 w-1/12">
+              <div className={`h-4 w-6 rounded animate-pulse ml-auto ${bgPulse}`}></div>
+            </td>
+            <td className="py-2 px-4 w-5/12">
+              <div className={`h-4 w-3/4 rounded animate-pulse ${bgPulse}`}></div>
+            </td>
+            <td className="py-2 px-4 w-5/12">
+              <div className={`h-4 w-full rounded animate-pulse ${bgPulse}`}></div>
+            </td>
+            <td className="py-2 px-4 w-1/12">
+              <div className={`h-4 w-8 rounded animate-pulse ml-auto ${bgPulse}`}></div>
+            </td>
+          </tr>
+        );
+      })}
+    </>
   );
 };
 
-interface Person {
-  id: number;
-  name: string;
-  skills: string;
-  score: number;
-  hourRate: string;
-}
-interface InfiniteScroll {
-  children: ReactNode;
-  loading: boolean;
-  dataLegth: number;
-}
-
-const InfiniteScroll: React.FC<InfiniteScroll> = ({
-  children,
-  loading,
-  dataLegth,
-}) => {
-  const handleScroll = (e: SyntheticEvent) => {
-    const target = e.target as HTMLElement;
-    const bottom =
-      target.scrollHeight === target.scrollTop + target.clientHeight;
-    if (bottom) {
-      // loadMoreData();
-    }
-  };
-
-  return (
-    <div className="overflow-auto max-h-96 " onScroll={handleScroll}>
-      {children}
-
-      <SkeletonRow tableCount={dataLegth + 1} />
-      {loading && <SkeletonRow tableCount={dataLegth + 1} />}
-    </div>
-  );
-};
-
-export default InfiniteScroll;
+export default SkeletonRow;
--- src/views/TeamTable.tsx
+++ src/views/TeamTable.tsx
@@ -1,98 +1,99 @@
-import InfiniteScroll from '../components/InfiniteScroll';
+import { SyntheticEvent, useState, } from 'react'
+import SkeletonRow from '../components/InfiniteScroll';
+const data = [
+  {
+    id: 1,
+    name: 'John',
+    score: 5,
+    skills: ['Frontend Developer', 'React', 'Angular'],
+    hourRate: 25,
+  },
+  {
+    id: 2,
+    name: 'Jane',
+    score: 4,
+    skills: ['node'],
+    hourRate: 30,
+  },
+  {
+    id: 3,
+    name: 'Doe',
+    score: 3,
+    skills: ['Fullstack Developer', 'React', 'NodeJS'],
+    hourRate: 35,
+  },
+  {
+    id: 4,
+    name: 'Smith',
+    score: 2,
+    skills: ['Fullstack Developer', 'Angular', 'Express'],
+    hourRate: 40,
+  },
+]
 
 const TeamTable = () => {
-  const data = [
-    {
-      id: 1,
-      name: 'John',
-      score: 5,
-      skills: ['Frontend Developer', 'React', 'Angular'],
-      hourRate: 25,
-    },
-    {
-      id: 2,
-      name: 'Jane',
-      score: 4,
-      skills: ['node'],
-      hourRate: 30,
-    },
-    {
-      id: 3,
-      name: 'Doe',
-      score: 3,
-      skills: ['Fullstack Developer', 'React', 'NodeJS'],
-      hourRate: 35,
-    },
-    {
-      id: 4,
-      name: 'Smith',
-      score: 2,
-      skills: ['Fullstack Developer', 'Angular', 'Express'],
-      hourRate: 40,
-    },
-  ];
+  const [items, setItems] = useState(data)
+  const [loading, setLoading] = useState(false)
+  const [ood, setOdd] = useState(items.length % 2)
+
+
+  const loadMoreItems = () => {
+    setLoading(true)
+    console.log('loading', loading)
+    setTimeout(() => {
+      const nextItems = data.slice(items.length, items.length + 2)
+      setItems((prevItems) => [...prevItems, ...nextItems])
+      setOdd(items.length % 2)
+      setLoading(false)
+      console.log('loading', loading)
+    }, 2000)
+  }
+
+  const handleScroll = (e: SyntheticEvent) => {
+    const target = e.target as HTMLElement;
+    const bottom =
+      target.scrollHeight === target.scrollTop + target.clientHeight;
+    if (bottom) {
+      loadMoreItems()
+    }
+  }
+
   return (
     <div className="container mx-auto p-4 bg-white border border-gray-200 border-1 rounded-sm">
-      <div className=" flex flex-wrap sm:justify-between pb-8 md:pb-14 gap-y-3 sm:gap-0 ">
-        <span className="text-black font-extrabold ">Team</span>
+      <div className="flex flex-wrap sm:justify-between pb-8 md:pb-14 gap-y-3 sm:gap-0">
+        <span className="text-black font-extrabold">Team</span>
       </div>
-
-      <div className="min-w-full">
-        <InfiniteScroll dataLegth={data.length} loading={false}>
-          <div className="w-full flex">
-            <div className="bg-slate-500 w-1/12">12</div>
-            <div className="bg-red-500 w-5/12">12</div>
-            <div className="bg-blue-500 w-5/12">12</div>
-            <div className="bg-green-500 w-1/12">12</div>
-          </div>
-          <table className=" w-full  ">
-            <thead>
-              <tr className="border-b-2 border-gray-300">
-                <th className="py-2 px-4 text-left text-sm font-medium text-gray-600 w-1/12">
-                  Score
-                </th>
-                <th className="py-2 px-4 text-left text-sm font-medium text-gray-600 w-5/12">
-                  Name
-                </th>
-                <th className="py-2 px-4 text-sm font-medium text-gray-600 w-5/12">
-                  Skills
-                </th>
-                <th className="py-2 pr-4 pl-20 text-left text-sm font-medium text-gray-600 w-1/12">
-                  Internal Price
-                </th>
+      <div className=' h-[100px] overflow-x-auto' onScroll={handleScroll}>
+        <table className="min-w-full table-fixed">
+          <thead className="">
+            <tr className='border-b-2 border-gray-300'>
+              <th className="py-2 px-4 w-1/12 text-left">Score</th>
+              <th className="py-2 px-4 w-5/12 text-left">Name</th>
+              <th className="py-2 px-4 w-5/12 text-left">Skills</th>
+              <th className="py-2 px-4 w-1/12 text-left">Hour Rate</th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.map(({ id, score, name, skills, hourRate }) => (
+              <tr key={id} className="odd:bg-white even:bg-gray-100">
+                <td className="py-2 px-4 text-right">{score}</td>
+                <td className="py-2 px-4">{name}</td>
+                <td className="py-2 px-4 truncate">
+                  {skills.join(', ')}
+                </td>
+                <td className="py-2 px-4 text-right">${hourRate}</td>
               </tr>
-            </thead>
-            <tbody>
-              {data.map(({ id, score, name, skills, hourRate }) => (
-                <tr
-                  key={id}
-                  className="even:bg-gray-100 odd:bg-white min-w-full flex-col"
-                >
-                  <td className="py-2 px-4 text-sm text-gray-700 text-right border border-red-300 w-1/12">
-                    {score}
-                  </td>
-                  <td className="py-2 px-4 text-sm text-gray-700 w-5/12">
-                    {name}
-                  </td>
-                  <td className="py-2 px-4 text-sm text-gray-700 truncate w-5/12">
-                    {skills}
-                  </td>
-                  <td className="py-2 px-4 text-sm text-gray-700 text-right border border-red-400 w-1/12">
-                    {hourRate}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </InfiniteScroll>
-        <div className="w-full flex">
-          <div className="bg-slate-500 w-1/12">12</div>
-          <div className="bg-red-500 w-5/12">12</div>
-          <div className="bg-blue-500 w-5/12">12</div>
-          <div className="bg-green-500 w-1/12">12</div>
-        </div>
+            ))}
+            {loading && (
+              <>
+                <SkeletonRow elementLength={items.length} />
+              </>
+            )}
+          </tbody>
+        </table>
       </div>
     </div>
   );
 };
+
 export default TeamTable;
