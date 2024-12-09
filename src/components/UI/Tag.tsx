8386cd2fd5afb277c9ef2657c53e74761223362f
MAC01 <mac01@MAC01s-MacBook-Air.local>
2024-12-09 12:45:27 -0600

    add styles

--- src/components/UI/Tag.tsx
+++ src/components/UI/Tag.tsx
@@ -1,26 +1,29 @@
 import { CloseCircleOutline, CloseCircle } from 'react-ionicons';
 import { twMerge } from 'tailwind-merge';
 import { useState } from 'react';
+
 const Tag = ({ text, styles }: { text: string; styles?: string }) => {
   const [isHoaver, setIsHoaver] = useState(false);
 
   return (
-    <span
-      className={twMerge(
-        `bg-green-500 text-white flex justify-between items-center px-2 w-20 rounded-sm cursor-pointer fill-white ${
-          styles ?? ''
-        }`,
-      )}
-      onMouseEnter={() => setIsHoaver(true)}
-      onMouseLeave={() => setIsHoaver(false)}
-    >
-      {text}
-      {isHoaver ? (
-        <CloseCircle color={``} />
-      ) : (
-        <CloseCircleOutline color={``} />
-      )}
-    </span>
+    <div>
+      <span
+        className={twMerge(
+          `bg-green-500 text-white flex gap-1 items-center px-2 rounded-sm cursor-pointer fill-white text-sm font-semibold ${
+            styles ?? ''
+          }`,
+        )}
+        onMouseEnter={() => setIsHoaver(true)}
+        onMouseLeave={() => setIsHoaver(false)}
+      >
+        {text}
+        {isHoaver ? (
+          <CloseCircle color={``} height="10px" width="10px" />
+        ) : (
+          <CloseCircleOutline color={``} height="16px" width="16px" />
+        )}
+      </span>
+    </div>
   );
 };
 
