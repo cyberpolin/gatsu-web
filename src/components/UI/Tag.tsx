e1fe1a9d7ccd6719814f1881c990434276e43b3f
MAC01 <mac01@MAC01s-MacBook-Air.local>
2024-12-10 10:01:33 -0600

    use hover css instead  hover js

--- src/components/UI/Tag.tsx
+++ src/components/UI/Tag.tsx
@@ -1,27 +1,27 @@
 import { CloseCircleOutline, CloseCircle } from 'react-ionicons';
 import { twMerge } from 'tailwind-merge';
-import { useState } from 'react';
 
 const Tag = ({ label, styles = '' }: { label: string; styles?: string }) => {
-  const [isHoaver, setIsHover] = useState(false);
-
   return (
-    <div>
-      <span
-        className={twMerge(
-          `bg-green-500 text-white flex gap-2 items-center px-2 py-1 rounded-md cursor-pointer fill-white text-md font-sm w-fit ${styles}`,
-        )}
-        onMouseEnter={() => setIsHover(true)}
-        onMouseLeave={() => setIsHover(false)}
-      >
-        {label}
-        {isHoaver ? (
-          <CloseCircle color={``} height="18px" width="18px" />
-        ) : (
-          <CloseCircleOutline color={``} height="20px" width="20px" />
-        )}
-      </span>
-    </div>
+    <span
+      className={twMerge(
+        `bg-green-500 text-white flex gap-1 items-center px-2 py-1 rounded-md cursor-pointer text-md font-sm w-fit ${styles}`,
+      )}
+    >
+      {label}
+      <div className="relative group" onClick={() => alert('')}>
+        <CloseCircle
+          cssClasses="absolute group-hover:opacity-100 opacity-0 !fill-white transition-opacity"
+          height="19px"
+          width="19px"
+        />
+        <CloseCircleOutline
+          cssClasses="group-hover:opacity-0 !text-white transition-opacity"
+          height="19px"
+          width="19px"
+        />
+      </div>
+    </span>
   );
 };
 
