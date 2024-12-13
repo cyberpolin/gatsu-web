a0b2d8b45f774e1bbba7a0a25610ac7774e3dc13
MAC01 <mac01@MAC01s-MacBook-Air.local>
2024-12-13 12:30:03 -0600

    add useformik to addMember

--- src/views/AddMember.tsx
+++ src/views/AddMember.tsx
@@ -2,8 +2,47 @@ import BaseInput from '../components/UI/BaseInput';
 import Tag from '../components/UI/Tag';
 import SubmitBTN from '../components/UI/SubmitBTN';
 import { useState } from 'react';
+import * as Yup from 'yup';
+import { useFormik } from 'formik';
+
+type Member = {
+  name: string;
+  lastname: string;
+  email: string;
+  hourlyRate: number;
+};
+const validationMenber = Yup.object({
+  name: Yup.string().required('First Name is required'),
+  lastname: Yup.string().required('Last Name is required'),
+  email: Yup.string()
+    .email('Invalid email address')
+    .required('Email is required'),
+  hourlyRate: Yup.number().required('Hourly rate is required'),
+});
+
 const AddMember = () => {
   const [skills, setSkills] = useState<string[]>([]);
+  const [checked, setChecked] = useState(false);
+  const formik = useFormik<Member>({
+    initialValues: {
+      name: '',
+      lastname: '',
+      email: '',
+      hourlyRate: 0,
+    },
+    validationSchema: validationMenber,
+    validateOnChange: false,
+    validateOnBlur: false,
+    onSubmit: (values, { resetForm }) => {
+      setChecked(true);
+      console.log(values);
+      setTimeout(() => {
+        resetForm();
+        setChecked(false);
+      }, 500);
+    },
+  });
+
   return (
     <div className="mx-auto px-8 bg-white border border-gray-100 rounded-md">
       <h1 className="text-xl font-medium mt-5 mb-6">Add team member</h1>
@@ -15,9 +54,33 @@ const AddMember = () => {
             password and login to your dashboard.
           </p>
           <div className="flex flex-col h-60 justify-between">
-            <BaseInput handleValue={console.log} placeholder="Name" />
-            <BaseInput handleValue={console.log} placeholder="Lastname" />
-            <BaseInput handleValue={console.log} placeholder="email" />
+            <BaseInput
+              value={formik.values.name}
+              handleValue={formik.handleChange}
+              onBlur={formik.handleBlur}
+              placeholder="Name"
+              name="name"
+              errorMessage={formik.errors.name}
+              check={checked}
+            />
+            <BaseInput
+              value={formik.values.lastname}
+              handleValue={formik.handleChange}
+              onBlur={formik.handleBlur}
+              placeholder="Lastname"
+              name="lastname"
+              errorMessage={formik.errors.lastname}
+              check={checked}
+            />
+            <BaseInput
+              value={formik.values.email}
+              handleValue={formik.handleChange}
+              onBlur={formik.handleBlur}
+              placeholder="Email"
+              name="email"
+              errorMessage={formik.errors.email}
+              check={checked}
+            />
           </div>
         </div>
         <div>
@@ -27,14 +90,14 @@ const AddMember = () => {
             add as many as you like, just be sure you are work ready on that
             skill.
           </p>
-          <BaseInput
+          {/* <BaseInput
             styles="placeholder:text-[11px] xs:placeholder:text-base"
             placeholder="Add a tag and press enter"
             handleValue={(newValue: string) => {
               const value = newValue.split(/[, ]+/);
               setSkills((oldValue) => [...oldValue, ...value]);
             }}
-          />
+          /> */}
           <div className="flex gap-2 mt-2 flex-wrap">
             {skills.map((skill, index) => (
               <Tag
@@ -56,9 +119,13 @@ const AddMember = () => {
             write the amount previous agreed.
           </p>
           <BaseInput
-            handleValue={console.log}
-            inputWidth="w-12"
+            value={formik.values.hourlyRate}
+            handleValue={formik.handleChange}
+            onBlur={formik.handleBlur}
             placeholder="10"
+            name="hourlyRate"
+            errorMessage={formik.errors.hourlyRate}
+            check={checked}
           />
         </div>
         <div className="flex justify-center xs:justify-end   flex-wrap py-5 items-center gap-y-4 md:gap-y-0 gap-x-2">
@@ -67,10 +134,7 @@ const AddMember = () => {
             handlesubmit={() => console.log('hola')}
             styles="bg-transparent text-black "
           />
-          <SubmitBTN
-            label="Add menber"
-            handlesubmit={() => console.log('hola')}
-          />
+          <SubmitBTN label="Add menber" handlesubmit={formik.handleSubmit} />
         </div>
       </div>
     </div>
