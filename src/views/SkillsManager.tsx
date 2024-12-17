dfb3270290c942f0fb712c7ce48bc43e178b8366
MAC01 <mac01@MAC01s-MacBook-Air.local>
2024-12-17 16:00:36 -0600

    create add tags

--- src/views/SkillsManager.tsx
+++ src/views/SkillsManager.tsx
@@ -7,27 +7,35 @@ const TagsManager = () => {
   const [skills, setSkills] = useState([]);
   const [isOpen, setIsOpen] = useState(false);
   const [currentSkill, setCurrentSkill] = useState({ id: '', name: '' });
-  const getskills = async () => {
+  async function getSkills() {
     try {
       const { data } = await fetch.get('/skills');
       setSkills(data);
     } catch (error) {
       console.error(error);
     }
-  };
+  }
   const deleteSkill = async (id: string) => {
     try {
       await fetch.delete(`/skills/${id}`);
-      getskills();
+      getSkills();
     } catch (error) {
       console.error(error);
     }
   };
   const updateSkill = async (id: string, name: string) => {
     try {
-      // await fetch.put(`/skills/${id}`, { name });
-      // getskills();
-      console.log('id', id, 'name', name);
+      await fetch.put(`/skills/${id}`, { name });
+      getSkills();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const postSkills = async (name: string) => {
+    try {
+      const { data } = await fetch.post('/skills', { name });
+      getSkills();
+      return data;
     } catch (error) {
       console.error(error);
     }
@@ -42,15 +50,15 @@ const TagsManager = () => {
     toogleModal();
   };
   useEffect(() => {
-    getskills();
+    getSkills();
   }, []);
   return (
     <div>
       <dialog
         open={isOpen}
-        className="z-1 bg-transparent backdrop-blur-sm absolute top-16 w-full h-full flex items-center justify-center"
+        className="bg-transparent backdrop-blur-sm w-full h-full content-center"
       >
-        <div className="rounded-md p-4 bg-gray-200 flex flex-col justify-evenly h-60 w-80">
+        <div className="mx-auto rounded-md p-4 bg-gray-200 flex flex-col justify-evenly h-60 w-80">
           <span>Please type your change</span>
           <BaseInput
             handleValue={(value) =>
@@ -74,30 +82,38 @@ const TagsManager = () => {
           </div>
         </div>
       </dialog>
-      <ul className="max-w-[500px] m-auto">
-        {skills.map((task: any) => (
-          <li
-            className="border p-3 flex justify-between items-center"
-            key={task.id}
-          >
-            {task.name}
-            <div className="flex gap-2">
-              <button
-                className="p-2 bg-red-500 rounded-md min-w-20 hover:bg-red-400"
-                onClick={() => deleteSkill(task.id)}
-              >
-                Delete
-              </button>
-              <button
-                className="p-2 bg-green-500 rounded-md min-w-20 hover:bg-green-400"
-                onClick={() => openModal(task.id)}
-              >
-                Edit
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
+      <div className="max-w-[500px] m-auto divide-y-8 divide-transparent">
+        <BaseInput
+          placeholder="Add a new skill"
+          handleValue={(value) => postSkills(value)}
+        />
+        <ul>
+          {skills.map((task: any) => (
+            <li
+              className="border p-3 flex flex-wrap justify-center sm:justify-between items-center"
+              key={task.id}
+            >
+              <span className="min-w-16 text-center sm:text-left">
+                {task.name}
+              </span>
+              <div className="flex gap-2">
+                <button
+                  className="p-2 bg-red-500 rounded-md min-w-20 hover:bg-red-400"
+                  onClick={() => deleteSkill(task.id)}
+                >
+                  Delete
+                </button>
+                <button
+                  className="p-2 bg-green-500 rounded-md min-w-20 hover:bg-green-400"
+                  onClick={() => openModal(task.id)}
+                >
+                  Edit
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
