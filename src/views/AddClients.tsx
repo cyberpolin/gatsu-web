c35c169ecd46526d8eb0f3dc095a0970b43b5df0
MAC01 <mac01@MAC01s-MacBook-Air.local>
2024-12-19 14:30:18 -0600

    improve styles

--- src/views/AddClients.tsx
+++ src/views/AddClients.tsx
@@ -4,6 +4,7 @@ import SubmitBTN from '../components/UI/SubmitBTN';
 import fetch from '../utils/fetch';
 import * as Yup from 'yup';
 import { useFormik } from 'formik';
+import { useState } from 'react';
 
 const validation = Yup.object({
   name: Yup.string().required('First Name is required'),
@@ -12,9 +13,11 @@ const validation = Yup.object({
     .required('Email is required'),
 });
 const AddClients = () => {
+  const [checked, setChecked] = useState(false);
   const SubmitClients = async (person: { name: string; email: string }) => {
     try {
       const { data } = await fetch.post('/clients', person);
+      console.log(data);
     } catch (error) {
       console.error(error);
     }
@@ -28,28 +31,45 @@ const AddClients = () => {
     validateOnChange: false,
     validateOnBlur: false,
     onSubmit: (values, { resetForm }) => {
+      setChecked(true);
       SubmitClients(values);
+      setTimeout(() => {
+        resetForm();
+        setChecked(false);
+      }, 500);
     },
   });
   return (
     <GeneralContainer title="Add Clients">
-      <BaseInput
-        placeholder="Clients Name"
-        handleValue={formik.handleChange}
-        onBlur={formik.handleBlur}
-        name="name"
-        value={formik.values.name}
-        errorMessage={formik.errors.name}
-      />
-      <BaseInput
-        placeholder="Clients Email"
-        handleValue={formik.handleChange}
-        onBlur={formik.handleBlur}
-        name="name"
-        value={formik.values.email}
-        errorMessage={formik.errors.email}
-      />
-      <SubmitBTN label="Save" handlesubmit={formik.handleSubmit}></SubmitBTN>
+      <div>
+        <span className="text-gray-800">Client information</span>
+        <p className="text-gray-400 text-xs mt-2 mb-8">
+          Please fill in the form to add a new client.
+        </p>
+        <div className="flex flex-col h-60 justify-between">
+          <BaseInput
+            placeholder="Clients Name"
+            handleValue={formik.handleChange}
+            onBlur={formik.handleBlur}
+            name="name"
+            value={formik.values.name}
+            errorMessage={formik.errors.name}
+            check={checked}
+          />
+          <BaseInput
+            placeholder="Clients Email"
+            handleValue={formik.handleChange}
+            onBlur={formik.handleBlur}
+            name="email"
+            value={formik.values.email}
+            errorMessage={formik.errors.email}
+            check={checked}
+          />
+          <div className="flex justify-end flex-wrap py-5 items-center gap-y-4 md:gap-y-0 gap-x-2">
+            <SubmitBTN label="save" handlesubmit={formik.handleSubmit} />
+          </div>
+        </div>
+      </div>
     </GeneralContainer>
   );
 };
