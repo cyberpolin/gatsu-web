cc00d278bb3a53a22ab5eeb4a3c0fdf24f1513c0
MAC01 <mac01@MAC01s-MacBook-Air.local>
2024-12-26 09:14:48 -0600

    add link to menu

--- src/MainLayout.tsx
+++ src/MainLayout.tsx
@@ -154,6 +154,21 @@ const Sidebar = ({ menuOpen, toggleMenu }: MenuProps) => {
       link: '/add-member',
     },
     {
+      title: 'Add Entries',
+      icon: (
+        <CodeOutline
+          style={{
+            width: '18px',
+            height: '18px',
+          }}
+          cssClasses={
+            '!text-green-500 mr-2 group-hover:translate-x-2 transition-all delay-100 duration-300 '
+          }
+        />
+      ),
+      link: '/entries',
+    },
+    {
       title: 'Log out',
       icon: (
         <OpenOutline
